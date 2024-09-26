1780dba5e39c7c6c091be98c623e45000ab293ff
KallosP <pkallos19@gmail.com>
2024-09-25 17:10:13 -0700

    IE1 first commit

---
+++ packages/react-frontend/src/MyApp.jsx
@@ -0,0 +1,43 @@
+// src/MyApp.jsx
+import React, { useState } from "react";
+import Table from "./Table";
+
+function MyApp() {
+  // Contains our data in the character state by the MyApp component
+  const [characters, setCharacters] = useState([
+    {
+      name: "Charlie",
+      job: "Janitor",
+    },
+    {
+      name: "Mac",
+      job: "Bouncer",
+    },
+    {
+      name: "Dee",
+      job: "Aspring actress",
+    },
+    {
+      name: "Dennis",
+      job: "Bartender",
+    },
+  ]);
+
+  function removeOneCharacter(index) {
+    const updated = characters.filter((character, i) => {
+      return i !== index;
+    });
+    setCharacters(updated);
+  }
+
+  return (
+    <div className="container">
+      <Table
+        characterData={characters}
+        removeCharacter={removeOneCharacter}
+      />
+    </div>
+  );
+}
+
+export default MyApp;
