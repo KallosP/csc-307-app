e3c4cf713dde77c0f81a16c51b78174651fbb6fb
KallosP <pkallos19@gmail.com>
2024-09-25 17:20:12 -0700

    feat: add form

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,27 +1,11 @@
 // src/MyApp.jsx
 import React, { useState } from "react";
 import Table from "./Table";
+import Form from "./Form";
 
 function MyApp() {
   // Contains our data in the character state by the MyApp component
-  const [characters, setCharacters] = useState([
-    {
-      name: "Charlie",
-      job: "Janitor",
-    },
-    {
-      name: "Mac",
-      job: "Bouncer",
-    },
-    {
-      name: "Dee",
-      job: "Aspring actress",
-    },
-    {
-      name: "Dennis",
-      job: "Bartender",
-    },
-  ]);
+  const [characters, setCharacters] = useState([]);
 
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
@@ -32,10 +16,11 @@ function MyApp() {
 
   return (
     <div className="container">
-      <Table
-        characterData={characters}
-        removeCharacter={removeOneCharacter}
+      <Table 
+        characterData={characters} 
+        removeCharacter={removeOneCharacter} 
       />
+      <Form />
     </div>
   );
 }
